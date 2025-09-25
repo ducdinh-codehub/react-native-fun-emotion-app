f1837ef87569c2f73f5aec68743f65a90758577f
Duc Dinh Anh <ducdinh97@Ducs-MacBook-Pro.local>
2025-09-25 10:53:47 +0700

    Adding gitignore file app.json babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+        root: ['./src/'],
+        alias: {
+          '@app': './src',
+          '@core': './src/core',
+          '@store': './src/store',
+          '@models': './src/models',
+          '@screens': './src/screens',
+          '@assets': './assets',
+          '@controllers': './src/controllers',
+        },
+      },
+      'react-native-reanimated/plugin',
+    ],
+    ['react-native-worklets/plugin'],
+  ],
   presets: ['module:@react-native/babel-preset'],
 };
